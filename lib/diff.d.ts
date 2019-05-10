9d9b61ec94c04c60aefdd6c73ce3c2236329f63e
Christian Brevik <christian.brevik@gmail.com>
2019-05-10 13:10:22 +0200

    Ignore lib folder

--- lib/diff.d.ts
+++
@@ -1,9 +0,0 @@
-export interface DiffLine {
-    text: string;
-    diffLineNumber: number;
-}
-export interface FileDiff {
-    path: string;
-    addedLines: DiffLine[];
-}
-export declare function getDiff(diff: string): FileDiff[];
--- lib/spellcheck.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Mispelled {
-    text: string;
-    start: number;
-    end: number;
-}
-export default function spellcheck(text: string): Array<Mispelled>;
